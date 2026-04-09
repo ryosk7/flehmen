4a029e42e1f12939ff8028fc487ec55b90e4e25c
ryosk7 <uchiryo7@gmail.com>
2026-04-09 12:26:01 +0900

    add entry point and CLI

---
+++ lib/synapse.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "fast_mcp"
+require_relative "synapse/version"
+require_relative "synapse/configuration"
+require_relative "synapse/model_registry"
+require_relative "synapse/field_filter"
+require_relative "synapse/query_builder"
+require_relative "synapse/serializer"
+require_relative "synapse/tools/list_models_tool"
+require_relative "synapse/tools/describe_model_tool"
+require_relative "synapse/tools/find_record_tool"
+require_relative "synapse/tools/search_records_tool"
+require_relative "synapse/tools/count_records_tool"
+require_relative "synapse/tools/show_associations_tool"
+require_relative "synapse/tools/execute_query_tool"
+require_relative "synapse/resources/schema_overview_resource"
+
+module Synapse
+  class << self
+    attr_accessor :model_registry
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    def reset_configuration!
+      @configuration = Configuration.new
+    end
+
+    def boot!
+      @model_registry = ModelRegistry.new(configuration)
+      @model_registry.discover!
+    end
+
+    def start_server!
+      server = FastMcp::Server.new(
+        name: "synapse",
+        version: Synapse::VERSION
+      )
+
+      server.register_tool(Tools::ListModelsTool)
+      server.register_tool(Tools::DescribeModelTool)
+      server.register_tool(Tools::FindRecordTool)
+      server.register_tool(Tools::SearchRecordsTool)
+      server.register_tool(Tools::CountRecordsTool)
+      server.register_tool(Tools::ShowAssociationsTool)
+      server.register_tool(Tools::ExecuteQueryTool) if configuration.enable_raw_sql
+
+      server.register_resource(Resources::SchemaOverviewResource)
+
+      server.start
+    end
+  end
+end
